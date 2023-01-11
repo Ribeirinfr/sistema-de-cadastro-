e963fecda23bb94ad8507c65df02e793193ef5f5
Ribeirinfr <117317498+Ribeirinfr@users.noreply.github.com>
2023-01-10 21:31:59 -0300

    atualizando o init do projeto

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.lod("aplicação está se conectando ao banco de dados")
+console.log("aplicação está se conectando ao banco de dados")
